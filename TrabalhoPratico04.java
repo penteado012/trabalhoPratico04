6b5bac17dd9e5711be9913bba5054c8cfc68a6c3
Matheus Penteado <161362483+penteado012@users.noreply.github.com>
2025-06-10 21:53:19 -0300

    Add files via upload

---
+++ TrabalhoPratico04.java
@@ -0,0 +1,17 @@
+/*
+Nome: Kaik Persike Maiorquino
+Prontuário: CB3029689
+
+Nome: Matheus Penteado de Barros
+Prontuário: CB3031501
+*/
+
+package trabalhopratico04;
+
+public class TrabalhoPratico04 {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
